91b315ba9b65922e29f008b49df52d88d49fb8fe
Juan Castillo <castillojuan1000@gmail.com>
2020-06-03 00:14:13 -0400

    setting up end points

--- index.js
+++ index.js
@@ -32,22 +32,48 @@ app.get('/todos', async (req, res) => {
   try {
     const allTodos = await pool.query("SELECT * FROM todo")
     res.json(allTodos.rows)
+
   } catch (error) {
     console.log(error.message)
   }
 })
 
 //get a todo
-app.get('todo/:id', (req, res) => {
+app.get('/todos/:id', async (req, res) => {
   try {
-
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo where todo_id = $1", [id])
+    res.json(todo.rows[0])
   } catch (error) {
     console.log(error.message)
   }
 })
+
+
 //update a todo
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+
+    const updateTodo = await pool.query("UPDATE todo set description = $1 WHERE todo_id = $2", [description, id])
+    res.json("todo was updated")
+  } catch (err) {
+    console.log(err.message);
+  }
+})
 
 //delete a todo
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo where todo_id = $1", [id])
+    res.json('Todo was deleted')
+
+  } catch (err) {
+    console.log(err.message)
+  }
+})
 
 
 app.listen(5000, () => {
